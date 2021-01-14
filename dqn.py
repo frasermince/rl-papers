c13e4965e55531f6b08d22034bf95f84ca58cbcf
frasermince <frasermince@gmail.com>
2021-01-14 12:15:06 -0600

    Divide input by 255 and use AdamW.

--- dqn.py
+++ dqn.py
@@ -38,33 +38,35 @@ class GameNet(nn.Module):
         super().__init__()
         #print("ACTION COUNT", action_count)
         self.conv1 = nn.Conv2d(4, 16, 8, 4)
-        self.conv1_bn = nn.BatchNorm2d(16)
+        # self.conv1_bn = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, 4, 2)
-        self.conv2_bn = nn.BatchNorm2d(32)
+        # self.conv2_bn = nn.BatchNorm2d(32)
         self.hidden_linear = nn.Linear(32 * 9 * 9, 256)
         self.output_layer = nn.Linear(256, action_count)
 
     def forward(self, x):
-        x = F.relu(self.conv1_bn(self.conv1(x)))
-        x = F.relu(self.conv2_bn(self.conv2(x)))
+        x = F.relu(self.conv1(x / 255))
+        x = F.relu(self.conv2(x))
         x = x.view(-1, 32 * 9 * 9)
         x = F.relu(self.hidden_linear(x))
         return self.output_layer(x)
 
 class DQN(LightningModule):
-    def __init__(self):
+    def __init__(self, learning_rate= 1e-4):
         super().__init__()
         self.env = gym.make("Pong-v0")
         self.env = wrappers.FrameStack(wrappers.ResizeObservation(wrappers.GrayScaleObservation(self.env), 84), 4)
         self.observation = self.env.reset()
-        self.epsilon = 0.99
+        self.epsilon = 1
         self.discount_factor = 0.99
         self.network = GameNet(self.env.action_space.n)
         self.target_network = GameNet(self.env.action_space.n)
         self.target_update_rate = 1000
         self.reward = 0
+        self.game_reward = 0
         self.games = 0
         self.epoch_length = 50000
+        self.learning_rate = learning_rate
 
     def forward(self, observation, net):
         result = net(observation).to(dtype=torch.float64)
@@ -75,6 +77,8 @@ class DQN(LightningModule):
         self.log("reward", self.reward, prog_bar=True, on_step=True)
         self.log("games", self.games, prog_bar=True, on_step=True)
         self.log("epsilon", self.epsilon, prog_bar=True, on_step=True)
+        self.log("lr", self.learning_rate, prog_bar=True, on_step=True)
+        self.log("game_reward", self.game_reward, prog_bar=True, on_step=True)
 
         (observation, reward, is_done, next_observation) = data
         with torch.no_grad():
@@ -91,9 +95,9 @@ class DQN(LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.01)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 1)
-        return [optimizer], [scheduler]
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
+        #scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 1)
+        return optimizer
 
 
     def play_step(self):
@@ -109,11 +113,14 @@ class DQN(LightningModule):
 
         second_observation, reward, is_done, info = self.env.step(action)
         self.reward += reward
+        self.game_reward += reward
         state_tuple = (self.observation, reward, is_done, second_observation)
         if self.epsilon > 0.1:
             self.epsilon -= 0.0000009
         if is_done:
+            self.logger.experiment.add_scalar("game_score", self.game_reward, self.games)
             self.observation = self.env.reset()
+            self.game_reward = 0
             self.games += 1
 
         self.memory.append(state_tuple)
@@ -136,25 +143,22 @@ class DQN(LightningModule):
                 yield (self.prepare_for_batch(first_observation), reward, is_done, self.prepare_for_batch(second_observation))
 
     def training_epoch_end(self, outputs):
-        self.logger.experiment.add_scalar("average_game_reward", self.reward / self.games, self.current_epoch)
+        if self.games != 0:
+            self.logger.experiment.add_scalar("average_game_reward", self.reward / self.games, self.current_epoch)
         self.logger.experiment.add_scalar("total_game_reward", self.reward, self.current_epoch)
         self.logger.experiment.add_scalar("game_count", self.games, self.current_epoch)
-        epoch_dictionary = {
-                "average_game_reward": self.reward / self.games,
-                "total_reward": self.reward,
-                "game_count": self.games
-                }
         self.games = 0
         self.reward = 0
 
     def train_dataloader(self):
         self.dataset = ExperienceSourceDataset(self.train_batch)
         self.memory = Memory(1000000)
-        for i in range(128):
+        for i in range(10000):
             self.play_step()
 
         return DataLoader(dataset=self.dataset, batch_size=32)
 
-lr_monitor = LearningRateMonitor(logging_interval='step')
 lightning_module = DQN()#.load_from_checkpoint("./lightning_logs/version_82/checkpoints/epoch=26-step=1316096.ckpt")
-Trainer(callbacks=[lr_monitor], progress_bar_refresh_rate=50, max_epochs=40).fit(lightning_module)
+trainer = Trainer(progress_bar_refresh_rate=50, max_epochs=40, auto_lr_find=True)
+#trainer.tune(lightning_module)
+trainer.fit(lightning_module)
