57c5fde88d7897666e27081f501844e5ae9fa5db
frasermince <frasermince@gmail.com>
2021-03-26 15:31:25 -0500

    half imports

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,11 +1,11 @@
-import gym
-import gym.wrappers as wrappers
-import torch.nn as nn
-import torch
-import torch.nn.functional as F
-from pytorch_lightning import Trainer, LightningModule
-import random
-import numpy as np
+# import gym
+# import gym.wrappers as wrappers
+# import torch.nn as nn
+# import torch
+# import torch.nn.functional as F
+# from pytorch_lightning import Trainer, LightningModule
+# import random
+# import numpy as np
 from pl_bolts.datamodules.experience_source import Experience, ExperienceSourceDataset
 from torch.utils.data import DataLoader
 from pytorch_lightning.callbacks import LearningRateMonitor
