c4829fc239ee35df5f85e4f693cd61697ae7f6c5
Fraser Mince <fraser@thoughtbot.com>
2021-03-26 17:13:06 -0500

    Is it wrapper?

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,7 +1,7 @@
 # import gym
 import gym.wrappers as wrappers
 
-import torch.nn as nn
+# import torch.nn as nn
 # import torch.nn.functional as F
 
 # from pytorch_lightning import Trainer, LightningModule
