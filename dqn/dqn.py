2da9b9ae4b5223d0a8d505ac8e06c48f95c88193
Fraser Mince <fraser@thoughtbot.com>
2021-03-26 16:50:43 -0500

    Try this combo.

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,9 +1,9 @@
 import gym
 import gym.wrappers as wrappers
 
-# import torch.nn as nn
-# import torch
-# import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
 # from pytorch_lightning import Trainer, LightningModule
 
